9151186163575dc313797f71b5bf84bcff425ef2
cunyv <2468697909@qq.com>
2021-01-23 20:43:51 +0800

    项目初始化，相关配置，启动测试

--- src/main/java/top/allhere/BlogApplication.java
+++ src/main/java/top/allhere/BlogApplication.java
@@ -1,13 +1,22 @@
 package top.allhere;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@MapperScan("top.allhere.mapper")
+@RestController
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@GetMapping("/name")
+	public String getName(){
+		return "zhangjian";
+	}
 }
